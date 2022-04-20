c10d24ab06f7926569a975738f960077933d4570
Lo Van Long <longlv@v-auto.com.vn>
2022-04-20 14:06:57 +0700

    fix

--- app/Http/Controllers/BhytController.php
+++ app/Http/Controllers/BhytController.php
@@ -98,7 +98,8 @@ class BhytController extends Controller
         $bhyt->soDienThoai = $request->soDienThoai;
         $bhyt->hoTen = $request->hoTen;
         $bhyt->maHoGd = $request->maHoGd;
-        // $bhyt->completed = $request->completed;
+        if($request->completed)
+        $bhyt->completed = $request->completed;
 
         $bhyt->save();
         return $bhyt;
